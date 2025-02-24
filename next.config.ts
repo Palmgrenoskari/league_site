21c7f680ee43ddf830768c7df976d6230ce6b5f1
Oskari Palmgren <jokpal@utu.fi>
2025-02-24 20:31:44 +0200

    allow ddragon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default config;
